163e5f8290b22df5b984be06b53178fe32aae63c
whathow <marwa@gmail.com>
2023-09-18 10:47:06 +0300

    creating table

--- answers.sh
+++ answers.sh
@@ -10,3 +10,12 @@ WHERE name = 'Basma';
 7.UPDATE Students
 SET Points = Points - 9
 WHERE name = 'Alex';
+10.INSERT INTO graduates (name,gender, age,graduation)
+SELECT name, gender, Age, Points
+FROM Students
+WHERE name = 'Layal';
+11.UPDATE graduates
+SET graduation = '2018-9-8'
+WHERE name = 'Layal';
+12.DELETE FROM Students
+WHERE name = 'Layal';
